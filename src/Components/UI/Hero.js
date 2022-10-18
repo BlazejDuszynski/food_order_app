2ef04100e68fa2cf2f5a5b5228d69c1006cd3041
Blazej Duszynski <bduszynski92@gmail.com>
2022-10-18 21:14:09 +0200

    text and styling added to hero section

--- src/Components/UI/Hero.js
+++ src/Components/UI/Hero.js
@@ -1,8 +1,13 @@
 import React from "react";
 import styles from "./Hero.module.css";
 
-const Hero = (props) => {
-  return <div className={styles.hero}></div>;
+const Hero = () => {
+  return (
+    <div className={styles.hero}>
+      <h1 className={styles.hero__header}>It's time to order some food!</h1>
+      <p className={styles.hero__text}>Choose your favourite dish and let us deliver it to your door!</p>
+    </div>
+  );
 };
 
 export default Hero;
