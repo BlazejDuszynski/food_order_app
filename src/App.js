a102f6d30d0268df1636406006ff4a18931e5c02
Blazej Duszynski <bduszynski92@gmail.com>
2022-10-18 21:01:37 +0200

    added background image to Hero

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import classes from "./App.css";
 
 import Header from "./Components/UI/Header";
+import Hero from "./Components/UI/Hero";
 
 function App() {
   return (
     <div className={classes.App}>
       <Header />
+      <Hero />
     </div>
   );
 }
