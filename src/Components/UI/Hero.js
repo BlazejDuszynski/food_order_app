8531c662fc9e98cb9201fefb88b618e452ff7238
Blazej Duszynski <bduszynski92@gmail.com>
2022-10-23 22:22:04 +0200

    Button which adds item to menu was styled

--- src/Components/UI/Hero.js
+++ src/Components/UI/Hero.js
@@ -4,7 +4,7 @@ import styles from "./Hero.module.css";
 const Hero = () => {
   return (
     <div className={styles.hero}>
-      <h1 className={styles.hero__header}>It's time to order some food!</h1>
+      <h1 className={styles.hero__header}>It's time to order food!</h1>
       <p className={styles.hero__text}>Choose your favourite dish and let us deliver it to your door!</p>
     </div>
   );
