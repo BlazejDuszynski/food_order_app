5b46eb3064e931fec68bfdffaf604d8c4ff0d27c
Blazej Duszynski <bduszynski92@gmail.com>
2022-11-10 18:44:45 +0100

    Cancel and confirm buttons added and styled in CartModal

--- src/Components/Layout/CartModal.js
+++ src/Components/Layout/CartModal.js
@@ -9,22 +9,38 @@ const CartModal = () => {
       <Card className={classes.cartModal}>
         <div>
           <header className={classes.header}>
-            <p className={classes.header__text}>your cart</p>
+            <p className={classes.header__text}>Your cart</p>
             <button className={classes.header__button}>X</button>
           </header>
           <ol>
-            <li>
+            <li className={classes.cartItem}>
               <div>Sushi</div>
               <div>25,00 PLN</div>
             </li>
-            <li>
+            <li className={classes.cartItem}>
               <div>Pizza</div>
               <div>25,00 PLN</div>
             </li>
           </ol>
-          <div>
+          <div className={classes.cartItem + " " + classes.cartSummary}>
             <p>Total amount</p>
             <p>50,00 PLN</p>
+          </div>
+          <div className={classes.cartButtons}>
+            <button
+              className={
+                classes.cartButtons__item + " " + classes.cartButtons__confirm
+              }
+            >
+              Confirm
+            </button>
+            <button
+              className={
+                classes.cartButtons__item + " " + classes.cartButtons__cancel
+              }
+            >
+              Cancel
+            </button>
           </div>
         </div>
       </Card>
